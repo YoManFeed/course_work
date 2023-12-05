ffe31a6301851ea3ddcb098a562b42d1895a9bf0
popit <mlgpavlin231@gmail.com>
2023-12-05 15:32:06 +0300

    полностью готовая программа, которая по введённой степени рисует шаблоны для внешних и внутренних дуг

--- draw cirlce and arches.py
+++ draw cirlce and arches.py
@@ -1,49 +1,7 @@
 import cv2
-from PIL import Image, ImageDraw
 import math
 from settings import *
 from working_with_images import show_me
-
-
-def draw_circle(x, y, radius, color="black"):
-    draw = ImageDraw.Draw(image)
-    draw.ellipse((x - radius, y - radius, x + radius, y + radius), outline=color, width=thickness)
-
-
-def draw_horizontal_line(x, y, length, angle, color="red", thickness=3):
-    draw = ImageDraw.Draw(image)
-    x2 = x + int(length * math.cos(math.radians(angle)))
-    y2 = y + int(length * math.sin(math.radians(angle)))
-
-    draw.line((x, y, x2, y2), fill=color, width=thickness)
-
-
-def draw_small_circle_up(x_dot, y_dot, r, alpha):
-    x = x_dot
-    y = y_dot
-    start_angle = 180 + math.degrees(alpha)
-    end_angle = 270 + math.degrees(alpha)
-
-    draw = ImageDraw.Draw(image)
-    draw.arc(
-        (x - r, y - r, x + r, y + r),
-        start=start_angle,
-        end=end_angle,
-        fill='black',
-        width=thickness,
-    )
-
-
-def draw_small_circle_bottom(x_dot, y_dot, r):
-    draw = ImageDraw.Draw(image)
-    draw.arc(
-        (x_dot - r, y_dot - 2 * r, x_dot+r, y_dot),
-        start=90,
-        end=180,
-        fill='black',
-        width=thickness,
-    )
-    return draw
 
 
 def generate_circle_points(center, radius, num_points):
@@ -56,19 +14,21 @@ def generate_circle_points(center, radius, num_points):
     return points
 
 
-def connect_circles(x, y, alpha, r, R):
-    R_c = (math.sqrt(R ** 2 + (r * R / (R + r)) ** 2) + math.sqrt(r ** 2 + (r ** 2 / (R + r)) ** 2))
+def connect_circles(image, x, y, alpha, r, R):
+    R_c = round((math.sqrt(R ** 2 + (r * R / (R + r)) ** 2) + math.sqrt(r ** 2 + (r ** 2 / (R + r)) ** 2)))
     phi = math.atan((r / (R + r)))
-    start_angle = 180 + math.degrees(phi)
-    end_angle = 180 + math.degrees(alpha) - math.degrees(phi)
-    draw = ImageDraw.Draw(image)
-    draw.arc(
-        (x-R_c, y-R_c, x+R_c, y+R_c),
-        start=start_angle,
-        end=end_angle,
-        fill='black',
-        width=thickness,
-    )
+    image = cv2.ellipse(image, (x, y), (R_c, R_c), 0, 180 + math.degrees(phi),
+                        180 + math.degrees(alpha) - math.degrees(phi), colour, 2)
+    return image
+
+
+def draw_inner_arch(image, x1, y1, x2, y2, i, alpha):
+    center = ((x1 + x2) // 2, (y1 + y2) // 2)
+    ro = math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2) / 2
+    axes = (int(ro), int(math.sqrt(5) * ro / (4*(i+1))))
+    angle = -75 + i * math.degrees(alpha / 2)
+    image = cv2.ellipse(image, center, axes, angle, 0, 180, colour, thickness)
+    return image
 
 
 if __name__ == '__main__':
@@ -76,54 +36,39 @@ if __name__ == '__main__':
     center = (image_size[0] // 2, image_size[1] // 2)
     thickness = 3
 
-    deg = 6
+    deg = 4
     num_points = 2 * deg
     R = image_size[0] // 4
     D = 2*R
     r = R // (deg+2)
-
-    # image = Image.new("RGB", image_size, "white")
-    # draw = ImageDraw.Draw(image)
-
-    points_on_circle = generate_circle_points(center, R, num_points)
-    points_on_circle = points_on_circle[num_points // 2:] + points_on_circle[:num_points // 2]
-
-    for i, point in enumerate(points_on_circle):
-        if i % 2 == 1:
-            angle = (i / num_points) * 2 * math.pi
-            r_upd = r + r * (i // 2)
-
-            image = Image.new("RGB", image_size, "white")
-            draw_circle(D, D, R, color='black')
-
-            draw_small_circle_bottom(R, D, r_upd)
-            draw_small_circle_up(point[0] - r_upd * math.sin(angle), point[1] + r_upd * math.cos(angle), r_upd, alpha=angle)
-            connect_circles(D, D, angle, r_upd, R)
-
-            # file_path = os.path.join()
-            # image.save(file_path)
-
-    x1=points_on_circle[0][0]
-    y1=points_on_circle[0][1]
-
-    x2=points_on_circle[1][0]
-    y2=points_on_circle[1][1]
-
-    draw_horizontal_line(x1,y1,5,0)
-    draw_horizontal_line(x2,y2,5,0)
-
-    image_np = np.array(image)
-    image_cv2 = cv2.cvtColor(image_np, cv2.COLOR_RGB2BGR)
-
-    center = ((x1 + x2) // 2, (y1 + y2) // 2)
-    ro = 33
-    # ro = math.sqrt((x1 - x2)**2 + (y1 - y2)**2)
-    print(ro)
-    axes = (ro, int(math.sqrt(5)*ro/4))
-    angle = -75  # Угол поворота в градусах
-    color = (0, 0, 0)
+    colour = (0, 0, 0, 255)
     thickness = 2
+    alpha = 2 * math.pi / num_points
 
-    cv2.ellipse(image_cv2, center, axes, angle, 0, 180, color, thickness)
+    points = generate_circle_points(center, R, num_points)
+    points = points[num_points // 2:] + points[:num_points // 2]
 
-    show_me(image_cv2)
+    n_channels = 4
+    blanc_img = np.zeros((image_size[0], image_size[1], n_channels), dtype=np.uint8)
+
+    for i, point in enumerate(points):
+        if i % 2 == 1:
+            angle = i * alpha
+            r_upd = r + r * (i // 2)
+            center = (round(point[0] - r_upd * math.sin(angle)), round(point[1] + r_upd * math.cos(angle)))
+
+            image = blanc_img.copy()
+            # image = cv2.ellipse(image, (D, D), (R, R), 0, 0, 360, (255, 255, 255), 2)
+
+            image = cv2.ellipse(image, (R, D - r_upd), (r_upd, r_upd), 0, 90, 180, colour, thickness)
+            image = cv2.ellipse(image, center, (r_upd, r_upd), 0, 180 + math.degrees(angle), 270 + math.degrees(angle), colour, thickness)
+            image = connect_circles(image, D, D, angle, r_upd, R)
+            # show_me(image)
+
+            cv2.imwrite(f"./external/deg_{deg}/external_arch_{i // 2 + 1}.png", image)
+
+            if i // 2 < deg // 2:
+                image = blanc_img.copy()
+                result = draw_inner_arch(image, points[0][0], points[0][1], point[0], point[1], i - 1, alpha)
+                cv2.imwrite(f"./inner/deg_{deg}/inner_arch_{i // 2 + 1}.png", image)
+                # show_me(result)
